b8d2adbc656f05921e21c4db78814044063ab952
sulfur <45544181+sulfur3206@users.noreply.github.com>
2019-10-16 16:49:09 -0700

    Add files via upload
    This is the motor map in header format

---
+++ map.h
@@ -0,0 +1,9 @@
+/*
+Motor: LeftDriveBack  		Port: 20 >
+Motor: LeftDriveForward  	Port: 19 > Ch3
+Motor: RightDriveBack 	 	Port: 10 }
+Motor: RightDriveForward  	Port: 09 } Ch2
+Motor: IntakeLeft  		Port     )
+Motor: IntakeRight  		Port     ) R1
+Motor: Tilt  			Port
+*/
